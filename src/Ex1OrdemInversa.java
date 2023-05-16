cec1aeadfb4abad972ef4a632a0f0c7c14f98bea
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-05-16 16:55:31 -0300

    exercicio ordem inversa

---
+++ src/Ex1OrdemInversa.java
@@ -0,0 +1,18 @@
+public class Ex1OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+        int contador = 0;
+
+        System.out.print("Vetor: ");
+        while (contador < (vetor.length)) {
+            System.out.print(vetor[contador] + " ");
+            contador++;
+        }
+
+        System.out.print("\nVetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
