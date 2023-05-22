6dc42c6d35fd1901500c41f2dc17debd16809f81
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-05-22 15:52:04 -0300

    README para Exercício 3 Numeros Aleatorio

---
+++ src/Ex3NumerosAleatorios.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Ex3NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatoriso = new int[20];
+
+        for (int i = 0; i < numerosAleatoriso.length; i ++) {
+            int numero = random.nextInt(100);
+            numerosAleatoriso[i] = numero;
+
+        }
+
+        System.out.print("Numeros aleatorios: ");
+        for (int numero : numerosAleatoriso) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nSucessores dos numeros aleatorios: ");
+        for (int numero: numerosAleatoriso) {
+            System.out.print((numero + 1) + " ");
+        }
+
+        System.out.print("\nAntecessores dos numeros aleatorios: ");
+        for (int numero: numerosAleatoriso) {
+            System.out.print((numero - 1) + " ");
+        }
+    }
+}
